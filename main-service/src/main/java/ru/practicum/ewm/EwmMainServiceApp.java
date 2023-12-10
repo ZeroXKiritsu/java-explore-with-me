1b5ac28bf9738c4e138f2781ffcbb28c19e591c7
zerox <zerox@zerosystem.ru>
2023-12-10 12:11:06 +0000

    Исправление переноса с другого устройства и дублирования имён классов

--- main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApp.java
+++ main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EWMMainServiceApp {
+public class EwmMainServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(EWMMainServiceApp.class, args);
+        SpringApplication.run(EwmMainServiceApp.class, args);
     }
 }
